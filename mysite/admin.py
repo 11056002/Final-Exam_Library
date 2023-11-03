ec308049a1e70b1bc012b5f17b726f0b34dba061
11056002 <11056002@ntub.edu.tw>
2023-11-03 18:07:24 +0800

    新增並區別章節(con1)、簡介(con2)，修正排版。

--- mysite/admin.py
+++ mysite/admin.py
@@ -4,16 +4,16 @@ from .models import Publisher, Book1, Author, LmssUser
 class PublisherAdmin(admin.ModelAdmin):
     list_display = ('name', 'img', 'addr')  # 调整列表显示顺序
 
-class Autho1rAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author_img', 'sex', 'age', 'tel')  # 调整列表显示顺序
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author_img', 'sex', 'age', 'tel')   # 调整列表显示顺序
 
 class Book1Admin(admin.ModelAdmin):
-    list_display = ('name', 'book_img', 'book_con', 'ISBN', 'translator', 'date', 'publisher', 'status')  # 调整列表显示顺序
+    list_display = ('name', 'book_img', 'book_con1', 'book_con2', 'ISBN', 'translator', 'date', 'publisher', 'status')  # 调整列表显示顺序
 
 class LmssUserAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'password')  # 调整列表显示顺序
 
 admin.site.register(Publisher, PublisherAdmin)
-admin.site.register(Author, Autho1rAdmin)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book1, Book1Admin)
 admin.site.register(LmssUser, LmssUserAdmin)
--- mysite/models.py
+++ mysite/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # 出版商
 class Publisher(models.Model):
-    img = models.TextField()
+    img = models.TextField(default='Publisher_img')
     name = models.CharField(max_length=64)  # 名稱
     addr = models.CharField(max_length=64)  # 地址
 
@@ -11,7 +11,7 @@ class Publisher(models.Model):
 
 # 作者分類
 class Author(models.Model):
-    author_img = models.TextField()
+    author_img = models.TextField(default='author_img')
     name = models.CharField(max_length=64)  # 姓名
     sex = models.CharField(max_length=4)  # 性別
     age = models.IntegerField(default=0)  # 年齡
@@ -21,15 +21,26 @@ class Author(models.Model):
 
 # 書籍分類
 class Book1(models.Model):
-    book_img = models.TextField()
+    book_img = models.TextField(default='book_img')
     name = models.CharField(max_length=64, null=True)  # 名稱
-    book_con = models.TextField()  # 簡介
+    book_con1 = models.TextField()  # 章節
     ISBN = models.CharField(max_length=64)  # 編號
     author = models.ManyToManyField(Author)  # 作者（多對多）
     translator = models.CharField(max_length=64)  # 譯者
     date = models.DateField(null=True, blank=True)  # 出版日期（可為空）
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)  # 出版商
-    status = models.CharField(max_length=10) # 狀態
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)  # 出版商 
+    book_con2 = models.TextField(default='context')  # 簡介(內容)的默認值  
+    
+    class Status(models.TextChoices):
+        AVAILABLE = '館藏中', '館藏中'
+        RESERVED = '預約中', '預約中'
+        ON_LOAN = '外借中', '外借中'
+
+    status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.AVAILABLE,
+    )
 
     def __str__(self):
         return self.name
